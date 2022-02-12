d0671a07639daf17daf43f7a985a824af1384cb3
zyh <835785342@qq.com>
2022-02-13 00:45:34 +0800

    增加商品评价

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/ItemsController.java
@@ -0,0 +1,87 @@
+package com.imooc.controller;
+
+import com.imooc.enums.YesOrNo;
+import com.imooc.pojo.*;
+import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.pojo.vo.CommentLevelCountsVO;
+import com.imooc.pojo.vo.ItemInfoVO;
+import com.imooc.pojo.vo.NewItemsVO;
+import com.imooc.service.CarouselService;
+import com.imooc.service.CategoryService;
+import com.imooc.service.ItemService;
+import com.imooc.utils.IMOOCJSONResult;
+import com.imooc.utils.PagedGridResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(value = "商品接口", tags = {"商品信息展示的相关接口"})
+@RestController
+@RequestMapping("items")
+public class ItemsController extends BaseController{
+
+    @Autowired
+    private ItemService itemService;
+
+
+    @ApiOperation(value = "查询商品详情", notes = "查询商品详情", httpMethod = "GET")
+    @GetMapping("/info/{itemId}")
+    public IMOOCJSONResult subCat(@ApiParam(name = "itemId", value = "商品id", required = true)
+                                  @PathVariable String itemId) {
+        if (StringUtils.isBlank(itemId)) {
+            return IMOOCJSONResult.errorMsg(null);
+        }
+        Items item = itemService.queryItemById(itemId);
+        List<ItemsImg> itemImgList = itemService.queryItemImgList(itemId);
+        List<ItemsSpec> itemSpecList = itemService.queryItemSpecList(itemId);
+        ItemsParam itemParams = itemService.queryItemParam(itemId);
+
+        ItemInfoVO itemInfoVO = new ItemInfoVO();
+        itemInfoVO.setItem(item);
+        itemInfoVO.setItemImgList(itemImgList);
+        itemInfoVO.setItemSpecList(itemSpecList);
+        itemInfoVO.setItemParams(itemParams);
+        return IMOOCJSONResult.ok(itemInfoVO);
+    }
+
+    @ApiOperation(value = "查询商品评价等级", notes = "查询商品评价等级", httpMethod = "GET")
+    @GetMapping("/commentLevel")
+    public IMOOCJSONResult commentLevel(@ApiParam(name = "itemId", value = "商品id", required = true)
+                                        @RequestParam String itemId) {
+        if (StringUtils.isBlank(itemId)) {
+            return IMOOCJSONResult.errorMsg(null);
+        }
+        CommentLevelCountsVO countsVO = itemService.queryCommentCounts(itemId);
+        return IMOOCJSONResult.ok(countsVO);
+    }
+
+    @ApiOperation(value = "查询商品评价", notes = "查询商品评价", httpMethod = "GET")
+    @GetMapping("/comments")
+    public IMOOCJSONResult comments(@ApiParam(name = "itemId", value = "商品id", required = true)
+                                    @RequestParam String itemId,
+                                    @ApiParam(name = "level", value = "评价等级", required = false)
+                                    @RequestParam Integer level,
+                                    @ApiParam(name = "page", value = "当前页数", required = false)
+                                    @RequestParam Integer page,
+                                    @ApiParam(name = "pageSize", value = "每页显示数", required = false)
+                                    @RequestParam Integer pageSize) {
+        if (StringUtils.isBlank(itemId)) {
+            return IMOOCJSONResult.errorMsg(null);
+        }
+        if(page==null){
+            page=1;
+        }
+        if(pageSize==null){
+            pageSize= COMMENT_PAGE_SIZE;
+        }
+
+        PagedGridResult grid = itemService.queryPagedComments(itemId, level, page, pageSize);
+        return IMOOCJSONResult.ok(grid);
+    }
+
+}
---
+++ foodie-dev-service/src/main/java/com/imooc/service/ItemService.java
@@ -0,0 +1,62 @@
+package com.imooc.service;
+
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
+import com.imooc.pojo.vo.CommentLevelCountsVO;
+import com.imooc.pojo.vo.ItemCommentVO;
+import com.imooc.utils.PagedGridResult;
+
+import java.util.List;
+
+public interface ItemService {
+
+    /**
+     * 根据商品ID查询详情
+     *
+     * @param itemId
+     * @return
+     */
+    Items queryItemById(String itemId);
+
+    /**
+     * 根据商品ID查询商品图片列表
+     *
+     * @param itemId
+     * @return
+     */
+    List<ItemsImg> queryItemImgList(String itemId);
+
+    /**
+     * 根据商品ID查询商品规格
+     *
+     * @param itemId
+     * @return
+     */
+    List<ItemsSpec> queryItemSpecList(String itemId);
+
+    /**
+     * 根据商品ID查询商品参数
+     *
+     * @param itemId
+     * @return
+     */
+    ItemsParam queryItemParam(String itemId);
+
+    /**
+     * 根据商品ID查询商品评价等级数量
+     *
+     * @param itemId
+     */
+    CommentLevelCountsVO queryCommentCounts(String itemId);
+
+    /**
+     * 根据商品ID查询商品评价(分页)
+     *
+     * @param itemId
+     * @param level
+     * @return
+     */
+    PagedGridResult queryPagedComments(String itemId, Integer level, Integer page, Integer pageSize);
+}
