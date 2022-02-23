ea16b413f51ba7f3ea1adf92be803d994ebbed23
zhangyuanhui <835785342@qq.com>
2022-02-24 00:07:05 +0800

    跨域

--- foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
@@ -12,15 +12,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @Api(value = "首页", tags = {"首页展示的相关接口"})
 @RestController
 @RequestMapping("index")
+@CrossOrigin
 public class IndexController {
 
     @Autowired
