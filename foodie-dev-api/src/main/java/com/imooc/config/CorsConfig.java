60aeed2b4acc4ae9aa4020a1ec6b938faf30d797
zhangyuanhui <835785342@qq.com>
2022-02-24 00:21:45 +0800

    跨域1

--- foodie-dev-api/src/main/java/com/imooc/config/CorsConfig.java
+++ foodie-dev-api/src/main/java/com/imooc/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
     public CorsFilter corsFilter(){
         // 1.添加cors配置信息
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://127.0.0.1:8080");
+        config.addAllowedOrigin("*");
 
         // 设置是否发送cookie信息
         config.setAllowCredentials(true);
